68daac3031a53bfe99f4cb8092b61f57eb1bb836
Thanassis Sergouniotis <tsergouniotis@gmail.com>
2017-01-10 15:13:19 +0200

    Change message

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ var Router       = require('router')
 var router = Router()
 router.get('/', function (req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-  res.end('Hello World!')
+  res.end('tsergouniotis node server is up and running')
 })
  
 var server = http.createServer(function(req, res) {
